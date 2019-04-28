2e347ce2e297ffd80de61b14b155e95c5f24a7e0
Krishna Chittur <kchittur@utexas.edu>
2019-04-28 14:45:10 -0500

    small fixes

--- scripts/ldap.py
+++ scripts/ldap.py
@@ -8,10 +8,10 @@ class LDAP(Exploit):
         print(f"Running LDAP attack against IP address {ip_address} with usernames {usernames} and passwords {passwords} on domain {self.domain}")
         common_usernames = ['root', 'admin', 'user']
         with open(usernames, 'r') as f:
-            provided_usernames = f.readlines()
+            provided_usernames = f.read().split('\n')
         usernames = common_usernames + provided_usernames
         with open(passwords, 'r') as f:
-            passwords = f.readlines()
+            passwords = f.read().split('\n')
         common_groups = ['admins', 'users']
         domainstring = ','.join(['dc='+x for x in self.domain.split('.')])
         # sample command: ldapsearch -H ldap://localhost -x -D cn=admin,ou=admins,dc=glauth,dc=com -w dogood -b dc=glauth,dc=com
