e15fbcf67eafdf2635944b6334172f501c40d58c
Melody Truong <mel.truong3@gmail.com>
2019-04-29 18:28:51 -0500

    update autonmap to look for only http-auth

--- scripts/autonmap.py
+++ scripts/autonmap.py
@@ -22,7 +22,7 @@ def autonmap(host_ips):
             for proto in nm[host].all_protocols():
                 lport = nm[host][proto].keys()
                 for port in lport:
-                    if port == 80 and not "Apache" in nm[host][proto][port][version]:
+                    if port == 80 and not "Apache" in nm[host][proto][version]:
                         continue
                     elif port in services and not port == 80:
                         open_services.append(services[port])
