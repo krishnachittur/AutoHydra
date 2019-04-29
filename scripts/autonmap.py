c270486c904bcbb7ec8d44e06b3cf1832f65020d
root <root@localhost.localdomain>
2019-04-29 19:38:10 -0400

    ignore http for now

--- scripts/autonmap.py
+++ scripts/autonmap.py
@@ -3,7 +3,7 @@ import nmap
 # function that creates endpoints of all host ip's scanned
 def autonmap(host_ips):
     nm = nmap.PortScanner()
-    nm.scan(hosts = host_ips, arguments= '-T4 --open -Pn -n -p 22,80,443,5432,389,636')
+    nm.scan(hosts = host_ips, arguments= '-T4 --open -A -Pn -n -p 22,80,443,5432,389,636')
 
     host_openings = {}
 
@@ -22,9 +22,9 @@ def autonmap(host_ips):
             for proto in nm[host].all_protocols():
                 lport = nm[host][proto].keys()
                 for port in lport:
-                    if port == 80 and not "Apache" in nm[host][proto][version]:
-                        continue
-                    elif port in services and not port == 80:
+                    #if port == 80 and not "Apache" in nm[host][host][version]:
+                    #    pass
+                    if port in services and not port == 80:
                         open_services.append(services[port])
         if open_services:
             host_openings[host] = open_services
